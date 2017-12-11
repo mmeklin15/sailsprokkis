29c55c1ea86079a25c047cc672989f611e2b9d49
Markus Meklin <Markus Meklin>
2017-12-12 00:09:01 +0200

    tinymce editor

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,1 +1,10 @@
 $('.ui.modal.login').modal('attach events', '.login.button');
+console.log("loading sticky");
+$('.ui.sticky').sticky({context: '#content', observeChanges:true, offset:50});
+console.log("sticky loaded");
+tinymce.init({
+    selector: '#newblogpost',
+    theme: 'modern',
+    plugins: 'print preview searchreplace autolink directionality visualblocks visualchars fullscreen image link media template codesample table charmap hr pagebreak nonbreaking anchor toc insertdatetime advlist lists textcolor wordcount imagetools contextmenu colorpicker textpattern help',
+    toolbar1: 'fontselect | formatselect | bold italic strikethrough forecolor backcolor | link | alignleft aligncenter alignright alignjustify  | numlist bullist outdent indent  | removeformat',
+});
